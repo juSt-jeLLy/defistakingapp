b656cd4f0f33778a06135a190639fba364db6e35
juSt-jeLLy <yagneshpatel931@gmail.com>
2024-11-20 19:53:47 +0530

    first commit

---
+++ src/contracts/Tether.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.8.0;
+contract Tether {
+    string public name = "Tether";
+    string public symbol = "USDT";
+    uint256 public totalSupply = 1000000000000000000000000;
+    uint8 public decimals = 18;
+}
